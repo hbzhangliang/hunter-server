a9cc3a020c76014a1b0428b051435847cc30f549
hbzhangliang <avonny@126.com>
2018-07-06 11:37:32 +0800

    #STORY filter生效

---
+++ src/main/java/cn/com/cubic/platform/hunter/controller/aop/LoginFilter.java
@@ -0,0 +1,40 @@
+package cn.com.cubic.platform.hunter.controller.aop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Liang.Zhang on 2018/7/6.
+ **/
+@Component
+public class LoginFilter implements Filter{
+
+    private static final Logger log = LoggerFactory.getLogger(LoginFilter.class);
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+
+        log.info("1111"+request.getRequestURI());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
