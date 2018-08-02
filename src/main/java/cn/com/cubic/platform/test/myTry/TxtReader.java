7de692be826478f5763f2733466a6eafa60b0e8f
hbzhangliang <avonny@126.com>
2018-08-02 15:03:36 +0800

    #STORY kindhearted

--- src/main/java/cn/com/cubic/platform/test/myTry/TxtReader.java
+++ src/main/java/cn/com/cubic/platform/test/myTry/TxtReader.java
@@ -41,6 +41,7 @@ public class TxtReader {
         String startStr="nickname",endStr="money-comment";
         List<Map<String,Object>> list=new ArrayList<>(200);
         int startPt=0,endPt=0;
+        Set<String> namesSet=new HashSet<>(100);
         while (strInfo.indexOf(startStr)>-1&&strInfo.indexOf(endStr)>-1){ //存在数据
             Map<String,Object> map=new HashMap<>(2);
             startPt=strInfo.indexOf(startStr)+10;
@@ -52,11 +53,26 @@ public class TxtReader {
             endPt=strInfo.substring(startPt).indexOf("</span>");
             String money=strInfo.substring(startPt,startPt+endPt);
             map.put("money",strToInt(money));
-            list.add(map);
+//            list.add(map);
             strInfo=strInfo.substring(startPt+endPt);
             if(name.indexOf("FOX")>-1){
                 break;
             }
+
+
+            if(namesSet.contains(name)){
+                for(Map<String,Object> pp:list){
+                    if(pp.get("name").toString().equals(name)){
+                         pp.put("money",Integer.valueOf(pp.get("money").toString())+strToInt(money));
+                         break;
+                    }
+                }
+            }
+            else {
+                list.add(map);
+                namesSet.add(name);
+            }
+
 
         }
         //排序功能
@@ -66,12 +82,16 @@ public class TxtReader {
                 Integer i1=strToInt(o1.get("money").toString());
                 Integer i2=strToInt(o2.get("money").toString());
                 return -i1.compareTo(i2);
-//                return o1.get("name").compareTo(o2.get("name"));
+//                return o1.get("name").toString().compareTo(o2.get("name").toString());
             }
         });
+
+        double sum=0;
         for(Map<String,Object>item:list){
+            sum+=(int)item.get("money");
             System.out.println(item.get("name")+":"+item.get("money"));
         }
+        System.out.println("sum is "+sum);
     }
 
 
