29357e8ef81b6e26ed6d718cb2cf20b303754632
hbzhangliang <avonny@126.com>
2018-08-08 15:43:00 +0800

    #STORY city 表的增删改查

---
+++ src/main/java/cn/com/cubic/platform/hunter/mysql/mapper/TBizCityMapper.java
@@ -0,0 +1,10 @@
+package cn.com.cubic.platform.hunter.mysql.mapper;
+
+import cn.com.cubic.platform.hunter.mysql.entity.TBizCity;
+import cn.com.cubic.platform.hunter.mysql.entity.TBizCityExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+public interface TBizCityMapper extends BaseMapper<TBizCity,TBizCityExample>{
+}
